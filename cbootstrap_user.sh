1fbec147daeb1b9cc126661070772d059ef9872d
Francis Luong (Franco) <fluong@salesforce.com>
2016-08-25 14:43:04 -0400

    fix rvm gpg key

--- cbootstrap_user.sh
+++ cbootstrap_user.sh
@@ -17,6 +17,7 @@ pyenv global 2.7.12
 if pyenv versions | grep "* 2.7.12"; then echo "PYENV SUCCESSFULLY INSTALLED"; fi
 pip install virtualenv
 
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source ~/.bash_profile
 gem install fpm
